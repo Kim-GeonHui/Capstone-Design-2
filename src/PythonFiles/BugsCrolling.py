4755ff344838058f577dc73faf0789f64e3eec5a
김건희 <tahm_kench@naver.com>
2022-11-21 21:03:18 +0900

    [src] : 장르별 벅스 크롤링 데이터 수집 및 정제

--- src/PythonFiles/BugsCrolling.py
+++ src/PythonFiles/BugsCrolling.py
@@ -13,12 +13,12 @@ import re
 title_list = []
 singer_list = []
 
-for i in range(1,131):
+for i in range(101,145):
   
   driver = wd.Chrome('C:\chromedriver')
   driver.maximize_window()
 
-  url = 'https://music.bugs.co.kr/newest/track/nrh?page=%d'%i
+  url = 'https://music.bugs.co.kr/newest/track/nindie?page=%d'%i
   h = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
   driver.get(url)
 
@@ -30,7 +30,7 @@ for i in range(1,131):
 
   isTrue = False;
   
-  for j in title:  
+  for j in title:
     j = re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z&!?$@()+-=.':;<>/*^%#_\s]", "", j.text);
     j = j.replace(u'\xa0',u'')
     title_list.append(j)
@@ -46,10 +46,11 @@ for i in range(1,131):
   driver.quit()
 
 total_list = []
-for j in range(6500):
+for j in range(2200):
   total_list.append([title_list[j], singer_list[j]]);
+  
 
-with open('hiphoptest.csv', 'w', encoding='CP949', newline='') as f:
+with open('indie3.csv', 'w', encoding='CP949', newline='') as f:
   writer = csv.writer(f)
   writer.writerow(['타이틀', '아티스트'])
   writer.writerows(total_list)
